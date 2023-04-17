3ebe99df495444bc5b24ca4edb498af0e0ff826c
Lev Maximov <lev.maximov@gmail.com>
2023-04-17 14:38:48 +0700

    two plots

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import sys
 from math import pi     
  
 import numpy as np                                               
+from scipy.constants import c
+
 from PyQt5 import QtWidgets, QtCore, uic
 from PyQt5.QtCore import pyqtSlot as slot
 import pyqtgraph as pg
@@ -16,19 +18,34 @@ Design, _ = uic.loadUiType('design.ui') # Подгружаем список ви
 import pandas as pd
 df1 = pd.read_csv('reference signal.lvm', sep='\t', skiprows=21, decimal=',')
 df2 = pd.read_csv('silicon wafer signal.lvm', sep='\t', skiprows=21, decimal=',')
-r = df1.scan; s = df2.scan
+r = df1.scan 
+s = df2.scan[:len(r)]
+ftt_E_1 = np.fft.fft(df1['scan'])
+freq_1 = np.fft.fftfreq(len(ftt_E_1), df1['X_Value'][1] - df1['X_Value'][0])
 
 def calc_y(r, v=-141, xmin=170, xmax=190):
     return np.unwrap(np.angle(np.fft.fft(np.roll(r, v))))[xmin:xmax]#-v*np.linspace(0, 2*pi, len(r))
+
+def calc_n(r, s, dr=-141, ds=-305, xmin=170, xmax=190):
+    ur = np.unwrap(np.angle(np.fft.fft(np.roll(r, dr))))+dr*np.linspace(0, 2*pi, len(r))
+    us = np.unwrap(np.angle(np.fft.fft(np.roll(s, ds))))+ds*np.linspace(0, 2*pi, len(s))
+    delta_ang = us - ur
+    n = (1-(c*delta_ang[1:]/(freq_1[1:]*2.08/1000*6.28)))
+#    return freq_1[:len(freq_1)//2], n1[:len(freq_1)//2]
+#    return np.unwrap(np.angle(np.fft.fft(np.roll(r, v))))[xmin:xmax]#-v*np.linspace(0, 2*pi, len(r))
+    return freq_1[xmin:xmax], n[xmin:xmax]
 
 class ExampleApp(QtWidgets.QMainWindow, Design):
     def __init__(self):
         super().__init__()
         self.setupUi(self)  # Этот метод из класса Design, он инициализирует виджеты
         self.x = np.arange(len(r))
-        minv, maxv, v = -200, 0, -141
+        minv, maxv, dr, ds = -200, 0, -141, -305
         y = calc_y(r)
-        self.curve = self.canvas.plot(y, pen=(31, 119, 180))
+        t, n = calc_n(r, s)
+        self.curve1 = self.canvas1.plot(y, pen=(31, 119, 180))
+        self.curve2 = self.canvas2.plot(n, pen=(31, 119, 180))
+        self.curve3 = self.canvas3.plot(n, pen=(31, 119, 180))
         self.v.setMinimum(minv)
         self.v.setMaximum(maxv)
         self.v.setValue(v)
@@ -45,11 +62,21 @@ class ExampleApp(QtWidgets.QMainWindow, Design):
         if xmax is None:
             xmax = self.xmax.value()
         y = calc_y(r, v, xmin, xmax)
-        self.curve.setData(y)
+        self.curve1.setData(y)
+        t, n = calc_n(r, s, dr=v, xmin=xmin, xmax=xmax)
+        self.curve2.setData(n)
 
     @slot(int)
     def on_v_valueChanged(self, v):
-        self.update(v)
+        self.update(v=v)
+
+    @slot(int)
+    def on_xmin_valueChanged(self, xmin):
+        self.update(xmin=xmin)
+
+    @slot(int)
+    def on_xmax_valueChanged(self, xmax):
+        self.update(xmax=xmax)
 
 if __name__ == '__main__':  # Если мы запускаем файл напрямую, а не импортируем
     app = QtWidgets.QApplication(sys.argv)  # Новый экземпляр QApplication
